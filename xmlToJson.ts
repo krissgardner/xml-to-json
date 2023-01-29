466b0601675c5a3dec85bf3a6b84b2bbacf6dadb
Kriss Gardner <krissrobert12@gmail.com>
2023-01-29 17:02:49 +0200

    add io files

--- xmlToJson.ts
+++ xmlToJson.ts
@@ -1,6 +1,7 @@
 // Considerations: xml-json package looks better, but requires too much process error handling and decided
 // against it
 import { readFileSync } from "fs";
+import {xml2json} from "xml-js";
 
 import {USAGE} from "./constants";
 
@@ -14,7 +15,7 @@ if (process.argv.length !== 3) {
 const fileName = String(process.argv[2]);
 
 // Read xml file
-const readFile = async (filename: string) => {
+const readFile = (filename: string) => {
     try {
         return readFileSync(`./${filename}.xml`, 'utf8');
     } catch (e) {
@@ -23,7 +24,8 @@ const readFile = async (filename: string) => {
     }
 }
 
+// Convert XML into JSON
 const xmlString = readFile(fileName);
+const jsonString = xml2json(xmlString, {compact: true});
+const jsonFile = JSON.parse(jsonString);
 
-
-// const result = xml2json(xml, {compact: true, spaces: 4});
